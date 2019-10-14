48b5ff21853585e4f3b30922d01f51551671d7bc
bellyk4real <bellyk4real@gmail.com>
2019-10-14 12:05:55 +0100

    added extensions to the app

--- naijasaver/app.py
+++ naijasaver/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
 from naijasaver.blueprints.user import user
+from naijasaver.extensions import (
+    db
+)
 
-def create_app():
+def create_app(settings_override=None):
     """
     Create a flask application using the app factory pattern
     
@@ -12,7 +15,23 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object('config.settings')
-    app.config.from_pyfile('setting.py', silent=True)
+    app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
 
     app.register_blueprint(user)
+    extensions(app)
+
     return app
+
+def extensions(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+
+    :param app: Flask application instance
+    :return: None
+    """
+    db.init_app(app)
+
+    return None
